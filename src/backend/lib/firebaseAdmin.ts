2f1aee6067f828258e24d53cdc350f59b31d4010
smv1256 <117322465+smv1256@users.noreply.github.com>
2025-08-11 16:49:17 -0400

    typo. ugh. build finally works. should've just tried locally instead of 50 commits but hey

--- src/backend/lib/firebaseAdmin.ts
+++ src/backend/lib/firebaseAdmin.ts
@@ -4,7 +4,7 @@ import { cert } from "firebase-admin/app";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }) 
