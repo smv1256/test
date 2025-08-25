20453eaeea80cf4d56c8dd3e06f4a5194e33812a
smv1256 <117322465+smv1256@users.noreply.github.com>
2025-08-24 21:40:01 -0400

    test

---
+++ src/app/auth/test/page.tsx
@@ -0,0 +1,9 @@
+export default function Test () {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold">Test</h1>
+      </main>
+    </div>
+  );
+} 
